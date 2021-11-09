1d9a4590748c17be54d4381728c772d823f7af0d
MiltonV <Vilchezmilton8@gmail.com>
2021-11-09 11:50:17 -0600

    Añadi la base.html y index.html de orders (Funcional)

--- pizza/urls.py
+++ pizza/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from orders.views import index
 
 urlpatterns = [
-    path("", include("orders.urls")),
+    #path("", include("orders.urls")),
     path("admin/", admin.site.urls),
+    path('', index), 
 ]
