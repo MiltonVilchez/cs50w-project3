4b125de6fd273c08f3b491712895f35e91e9b2e0
MiltonV <Vilchezmilton8@gmail.com>
2021-11-14 10:30:45 -0600

    Añadi todo el menu, modifique algunas medidas en los modelos - Elimine la relacion manytomany de las pizzas con los toppings

--- orders/models.py
+++ orders/models.py
@@ -12,7 +12,7 @@ class DinnerPlates (models.Model):
 
 class Pasta (models.Model):
     name_pasta = models.CharField(max_length=40, blank=True)
-    price_pasta = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
+    price_pasta = models.DecimalField(max_digits=8, decimal_places=6, blank=True)
 
 class Salads (models.Model):
     name_salads = models.CharField(max_length=40, blank=True)
@@ -23,40 +23,40 @@ class ExtraSubs(models.Model):
     price_extra_sub = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
 
 class Subs (models.Model):
-    extra_sub = models.ManyToManyField(ExtraSubs)
+    #extra_sub = models.ManyToManyField(ExtraSubs)
     name_subs = models.CharField(max_length=40, blank=True)
     tamanio = models.CharField(max_length=10, blank=True)
-    price_subs = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
+    price_subs = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
     
 class RegularPizza(models.Model):
     name_reg_pizza = models.CharField(max_length=30, blank=True)
     tamanio = models.CharField(max_length=10, blank=True)
-    price_reg_pizza = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
-    quantity_top = models.IntegerField(max_length=1, blank=True, unique=True)
-    toppings = models.ManyToManyField(Toppings)
+    price_reg_pizza = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    quantity_top = models.IntegerField(max_length=1, blank=True)
+    #toppings = models.ManyToManyField(Toppings)
 
 class SicilianPizza(models.Model):
     name_sic_pizza = models.CharField(max_length=30, blank=True)
     tamanio = models.CharField(max_length=10, blank=True)
-    price_sic_pizza = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
-    quantity_top = models.IntegerField(max_length=1, blank=True, unique=True)
-    toppings = models.ManyToManyField(Toppings)
+    price_sic_pizza = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    quantity_top = models.IntegerField(max_length=1, blank=True)
+    #toppings = models.ManyToManyField(Toppings)
 
 class SpecialRegularPizza(models.Model):
     name_special_reg = models.CharField(max_length=40 ,default="Special Regular Pizza")
     tamanio = models.CharField(max_length=10, blank=True)
-    price_special_reg = models.DecimalField(max_digits=5, decimal_places=4, blank=True)
-    toppings = models.ManyToManyField(Toppings)
+    price_special_reg = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    #toppings = models.ManyToManyField(Toppings)
 
 class SpecialSicilianPizza(models.Model):
     name_special_sic = models.CharField(max_length=40 ,default="Special Sicilian Pizza")
     tamanio = models.CharField(max_length=10, blank=True)
-    price_special_sic = models.DecimalField(max_digits=5, decimal_places=4,blank=True)
-    toppings = models.ManyToManyField(Toppings)
+    price_special_sic = models.DecimalField(max_digits=5, decimal_places=2,blank=True)
+    #toppings = models.ManyToManyField(Toppings)
 
 class Carrito(models.Model):
-    items = models.CharField(max_length=100, blank=True)
-    price_items = models.DecimalField(max_digits=6, decimal_places=4, blank=True)
+    items = models.CharField(max_length=200, blank=True)
+    price_items = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
     fecha_items = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
