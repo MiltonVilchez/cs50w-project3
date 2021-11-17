c206f2dfcb3892458b01678b702e1ff2e0b8c058
MiltonV <Vilchezmilton8@gmail.com>
2021-11-16 23:57:12 -0600

    Login listo

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
+from django.contrib import messages
 # Create your views here.
 def index(request):
-    return render(request, 'orders/index.html')
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/menu', request)
+        else:
+            messages.error(request, "Upss, We have an error. Please check if you entered the correct data")
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/menu', request)
+    else:
+        return render(request, 'orders/index.html')    
 
 def menu(request):
     
