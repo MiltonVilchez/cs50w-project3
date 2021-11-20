321f29dd6c86030cf7e528c280c8fa07a2e5092d
MiltonV <Vilchezmilton8@gmail.com>
2021-11-19 21:54:14 -0600

    No se porque putas no me carga el js cada vez que lo modifico

--- static/code/coding.js
+++ static/code/coding.js
@@ -1,8 +1,21 @@
 var modal = document.getElementById("exampleModal");
-    console.log(modal);
-    modal.addEventListener('show.bs.modal', function (event) {
-        var button = event.relatedTarget;
-        console.log(button);
-        var recipient = button.getAttribute('data-whatever');
-        modal.querySelector(".modal-title").innerHTML = recipient;       
-    })
+modal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var recipient = button.getAttribute('data-whatever');
+    modal.querySelector(".modal-title").innerHTML = recipient;
+    modal.querySelector(".otherTitle").innerHTML = recipient;
+});
+
+function Desabilita(esto) {
+    var numcheckbox = 0;
+    if (esto.checked) {
+      if (numcheckbox >= 3) {
+        esto.checked = false;
+        alert("Son solo 3 Toppings");
+      } else {
+        numcheckbox++;
+      }
+    } else {
+      numcheckbox--;
+    }
+  }
