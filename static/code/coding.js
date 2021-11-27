0e2ffad2d40de1dd0113f8724f49d85c4a3fb288
MiltonV <Vilchezmilton8@gmail.com>
2021-11-26 23:49:16 -0600

    Cambie si es special agregar 6 toppings

--- static/code/coding.js
+++ static/code/coding.js
@@ -2,9 +2,13 @@ var modal = document.getElementById("exampleModal");
 modal.addEventListener('show.bs.modal', function (event) {
     var button = event.relatedTarget;
     var recipient = button.getAttribute('data-whatever');
+    var type = button.getAttribute('data-type');
     modal.querySelector(".modal-title").innerHTML = recipient;
     modal.querySelector(".otherTitle").innerHTML = recipient + " - Chesse";
     modal.querySelector(".valueTitle").value = recipient + " - Chesse";
+    if (type == "Special"){
+      modal.querySelector(".letters").innerHTML = "If you want, you can add 6 toppings:"; 
+    }
 });
 
 function Desabilita(esto) {
